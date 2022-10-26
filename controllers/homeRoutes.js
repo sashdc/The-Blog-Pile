684097cde790c34b80abddf18ce80860ecad9eae
saharsh <saharshdc@gmail.com>
2022-10-26 08:10:19 -0400

    added login page and route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -14,10 +14,10 @@ router.get('/', async (req, res) => {
           },
         ],
       });
-  
+  console.log(postData)
       // Serialize data so the template can read it
       const posts = postData.map((post) => post.get({ plain: true }));
-  
+  console.log (posts)
       // Pass serialized data and session flag into template
       res.render('homepage', { 
         posts, 
@@ -27,6 +27,14 @@ router.get('/', async (req, res) => {
     }
   });
   
-
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/dashboard');
+      return;
+    }
+  
+    res.render('login');
+  });
 
   module.exports = router;
