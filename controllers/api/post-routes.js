bc94ead3fef716daeb4a08594b5d4e07af75b984
saharsh <saharshdc@gmail.com>
2022-11-06 20:53:07 -0500

    started cleaning up code

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -2,33 +2,6 @@ const router = require('express').Router();
 const { Post, User, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-// Get all posts
-router.get("/", (req, res) => {
-  Post.findAll({
-    attributes: ["id", "post_content", "post_title", "createdAt"],
-    order: [["createdAt", "DESC"]],
-    include: [
-      {
-        model: User,
-        attributes: ["username"],
-      },
-      {
-        model: Comment,
-        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-        include: {
-          model: User,
-          attributes: ["username"],
-        },
-      },
-    ],
-  })
-    .then((dbPostData) => res.json(dbPostData))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
 // Create a post when logged in
 router.post("/", withAuth, (req, res) => {
   console.log('trying to create post')
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -68,57 +68,5 @@ router.post("/logout", (req, res) => {
   }
 });
 
-// list all registered users
-router.get('/', (req, res) => {
-  User.findAll({
-          attributes: {
-              exclude: ['password']
-          }
-      })
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-      });
-});
-
-// Get specific user by id with content
-router.get('/:id', (req, res) => {
-  User.findOne({
-          attributes: {
-              exclude: ['password']
-          },
-          where: {
-              id: req.params.id
-          },
-          include: [{
-                  model: Post,
-                  attributes: ['id', 'post_title', 'post_content', 'createdAt']
-              },
-              {
-                  model: Comment,
-                  attributes: ['id', 'comment_text', 'created_at'],
-                  include: {
-                      model: Post,
-                      attributes: ['post_title']
-                  }
-              }
-          ]
-      })
-      .then(dbUserData => {
-          if (!dbUserData) {
-              res.status(404).json({
-                  message: 'No user found with this id, please try again'
-              });
-              return;
-          }
-          res.json(dbUserData);
-      })
-      .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-      });
-});
-
 
 module.exports = router;
